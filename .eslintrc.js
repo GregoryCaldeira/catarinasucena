e49acc938d5bdbf2c8d4c2d116cd3225c520256f
Gregory Caldeira <gregorycaldeira@MacBook-Pro-de-Gregory.local>
2022-10-07 16:55:32 +0100

    config project

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'standard-with-typescript',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['react'],
+  rules: {},
+};
